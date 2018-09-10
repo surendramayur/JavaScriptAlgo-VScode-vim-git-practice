16352204d4336d5b13118ef54843769a25934f84
Mayur <surendramayur@gmail.com>
2018-09-10 22:02:19 +0530

    using regex to solve vowels algo

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -8,16 +8,20 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    let count = 0;
-    const checker = ['a', 'e', 'i', 'o', 'u'];
-
-    for (let char of str.toLowerCase()) {
-        if (checker.includes(char)) {
-            count++;
-        }
-    }
-
-    return count;
+    const matches = str.match(/[aeiou]/gi);
+    return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
+
+
+/* let count = 0;
+const checker = ['a', 'e', 'i', 'o', 'u'];
+
+for (let char of str.toLowerCase()) {
+    if (checker.includes(char)) {
+        count++;
+    }
+}
+
+return count; */
