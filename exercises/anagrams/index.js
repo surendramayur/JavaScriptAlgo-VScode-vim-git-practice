c5f6d976d46dfb88120b564a943b0c7654e34a52
Mayur <surendramayur@gmail.com>
2018-09-10 13:05:27 +0530

    alernative solution to anagram

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -9,7 +9,29 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-    const aCharMap = buildCharMap(stringA);
+   return cleanString(stringA) === cleanString(stringB);
+}
+
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+
+}
+
+
+/* function buildCharMap(str) {
+    const charMap = {};
+
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+} */
+
+module.exports = anagrams;
+
+
+
+/*  const aCharMap = buildCharMap(stringA);
     const bCharMap = buildCharMap(stringB);
 
     if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
@@ -23,18 +45,4 @@ function anagrams(stringA, stringB) {
     }
 
     return true;
-
-}
-
-
-
-function buildCharMap(str) {
-    const charMap = {};
-
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
-    return charMap;
-}
-
-module.exports = anagrams;
+ */
