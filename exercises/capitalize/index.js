5876fb25fd81d1152e52a5d185aa4d465fa97e5f
Mayur <surendramayur@gmail.com>
2018-09-10 16:54:01 +0530

    alternate for loop for capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,13 +8,26 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
-    const words = [];
+    let result = str[0].toUpperCase();
 
-    for (let word of str.split(' ')) {
-        words.push(word[0].toUpperCase() + word.slice(1));
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === ' ') {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
     }
 
-    return words.join(' ');
+    return result;
 }
 
 module.exports = capitalize;
+
+/* const words = [];
+
+for (let word of str.split(' ')) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+}
+
+return words.join(' ');
+ */
