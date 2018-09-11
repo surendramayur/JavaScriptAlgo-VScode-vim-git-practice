1ff5452b0a9fb1815487c5a43e1faf9148120974
Mayur <surendramayur@gmail.com>
2018-09-11 18:29:58 +0530

    handling exponential runtime complexity due to recursion with memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,13 +8,33 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        if (cache[args]) {
+            return cache[args];
+        }
+
+        const result = fn.apply(this, args);
+        cache[args] = result;
+
+        return result;
+    };
+}
+
+
+
+
+
+function slowfib(n) {
    if (n < 2) {
        return n;
    }
 
    return fib(n -1) + fib(n - 2);
 }
+
+const fib = memoize(slowfib);
 
 module.exports = fib;
 
