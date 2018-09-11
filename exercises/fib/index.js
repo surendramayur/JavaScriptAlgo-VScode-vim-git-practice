1d3df8c4aeb16c680bcf4e28e78124ecbf6be82b
Mayur <surendramayur@gmail.com>
2018-09-11 12:50:49 +0530

    fibonacci algo with linear runtime complexity

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,17 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    const result = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        const a = result[i - 1]; //same as [result.length - 1];
+        const b = result[i - 2];
+
+        result.push(a + b);
+    }
+
+    return result[n]; //same as result.length - 1
+}
 
 module.exports = fib;
