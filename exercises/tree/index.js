c68b7a86bd048840c1e6b6e42d9421e2e715b882
Mayur <surendramayur@gmail.com>
2018-09-18 18:33:28 +0530

    traverse through a tree data structure BF/DF

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -27,6 +27,30 @@ class Node {
     }
 }
 
-class Tree {}
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+
+    traverseBF(fn) {
+        const arr = [this.root];
+        while(arr.length) {
+            const node = arr.shift();
+
+            arr.push(...node.children);
+            fn(node);
+        }
+    }
+
+    traverseDF(fn) {
+        const arr = [this.root];
+        while(arr.length) {
+            const node = arr.shift();
+
+            arr.unshift(...node.children);
+            fn(node);
+        }
+    }
+}
 
 module.exports = { Tree, Node };
